68b2a34b0647e7b73ef55fde652d63f4656d376d
zhangyx_xyz <zhangyx_xyz@outlook.com>
2016-09-16 21:49:49 +0800

    添加项目文件。

---
+++ StrategyPatternDemo(策略模式)/BicycleStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternDemo_策略模式_
+{
+    class BicycleStrategy : ITravelStrategy
+    {
+        public void TravelAlgorithm()
+        {
+            Console.WriteLine("Bicycle is routing means of transportation!");
+        }
+    }
+}
---
+++ StrategyPatternDemo(策略模式)/TrainStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternDemo_策略模式_
+{
+    class TrainStrategy : ITravelStrategy
+    {
+        public void TravelAlgorithm()
+        {
+            Console.WriteLine("Train is routing means of transportation!");
+        }
+    }
+}
